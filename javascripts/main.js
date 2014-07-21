c79d1271cccc62dc9b945a959e154168db24c562
Emil Soman <emil.soman@gmail.com>
2014-07-21 18:22:17 +0530

    Link and click event added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
 console.log('This would be the main JS file.');
+
+$(document).ready(function(){
+  $('#the-link').on('click', function(e){
+    e.preventDefault();
+    console.log('DEFAULT ACTION');
+  });
+})
