158bd71f9f35f4bc3b7d0559a5a6d4776127b8f0
Emil Soman <emil.soman@gmail.com>
2014-07-22 11:47:14 +0530

    Add variation code

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,10 @@ console.log('This would be the main JS file.');
 $(document).ready(function(){
   $('#the-link').on('click', function(e){
     e.preventDefault();
-    console.log('DEFAULT ACTION');
+    if(window.optimizelySingleForm) {
+      console.log('VARIATION!');
+    } else {
+      console.log('DEFAULT ACTION');
+    }
   });
 })
